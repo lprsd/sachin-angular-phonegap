faf59c4b23e0e585cb02da616860cea20ab061a9
Lakshman Prasad <lakshman@lakshmanprasad.com>
2013-11-18 20:15:19 +0530

    sachin-angular-phonegap

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('feApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ui.bootstrap'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('feApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'Phonegap',
+      'Bootstrap 3.0.2'
+    ];
+  });
