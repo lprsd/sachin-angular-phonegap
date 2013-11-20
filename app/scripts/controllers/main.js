829c780ca67ad81221608c2c228a8259c4ad1f7a
Aditya Sethi <aditya_sethi@external.mckinsey.com>
2013-11-20 13:22:09 +0530

    First SRT APP Push

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,4 +9,8 @@ angular.module('feApp')
       'Phonegap',
       'Bootstrap 3.0.2'
     ];
+
+    $scope.colors = ['#334D5C','#45b29d','#EFC94C','#E27A3F','#DF5A49'];
+    $scope.tiles = ['Sachin Stats', 'Fun Facts', 'Awesome Things', 'More Awesome Things'];
+
   });
