677150796e34aea77a634022e465f68c6be6d23e
Lakshman Prasad <lakshman@lakshmanprasad.com>
2013-12-21 14:03:05 +0530

    The first sachin chart.

--- app/scripts/controllers/SachinStatsCtrl.js
+++ app/scripts/controllers/SachinStatsCtrl.js
@@ -40,13 +40,39 @@ function getWonLost(matches, PieChartOptions){
 	return chart_data;
 }
 
+var colorsArray = ['#25ADA7','#A1D87F','#FF453C','#EFC94C','#AF709A','#FFD530', '#0E229B', '#A4A1CC','#25ADA7','#A1D87F','#FF453C','#EFC94C','#AF709A','#FFD530', '#0E229B', '#A4A1CC'];
+
+function get_series_data(Data, api_data, chosen_json, chosen_attr){
+
+    var series_data = []
+    var total_this_stat = $.map(api_data[chosen_json], function(x){return +x.Runs}).reduce(function(previousValue, currentValue){ return previousValue + currentValue;})
+    console.log(total_this_stat);
+    var i = 0;
+    for(var each_stat in api_data[chosen_json]){
+        var s = api_data[chosen_json][each_stat];
+        series_data.push({
+            name : s.name,
+            y :+(+(s[chosen_attr])/total_this_stat*100).toFixed(1),
+            color : colorsArray[i++]
+        });
+    }
+    console.log(series_data);
+    return series_data;
+}
+
+
 
 function plot_odi_stat_data($scope, Data, chart_options){
 
-    var chosen_json = 'vs Country';
-    var chosen_attr = 'runs';
+    var chosen_json = 'v Country';
+    var chosen_attr = 'Runs';
+    var api_data = $scope.r_api_data;
 
-    var series_data;
+    var series_data = get_series_data(Data,api_data,chosen_json,chosen_attr);
+    var chart_data = angular.copy(chart_options.pos);
+    chart_data.series = [{name:chosen_json, type: 'pie', data:series_data}];
+    chart_data.title.text = 'chosen_json';
+    $scope.chosenStat = chart_data;
 }
 
 
@@ -72,7 +98,7 @@ angular.module('app.controllers')
             });
 
             Data.get_local('scripts/lib/sachin_odi_summary.json').success(function(api_data){
-
+                $scope.r_api_data = api_data;
                 plot_odi_stat_data($scope, Data, PieChartOptions)
 
             });
