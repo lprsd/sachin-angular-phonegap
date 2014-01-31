b9b418c0bf42113f72761f8e769189cc0ef8a4f2
Aditya Sethi <aditya_sethi@external.mckinsey.com>
2014-01-31 18:53:50 +0530

    Lifetime chart orientation changes

--- app/scripts/services/data.js
+++ app/scripts/services/data.js
@@ -784,10 +784,10 @@ angular.module('app.services').factory('AreaChartOptions',[function ($http){
                 shared: true
             },
             legend: {
-                layout: 'vertical',
+                layout: 'horizontal',
+                floating: true,
                 align: 'right',
                 verticalAlign: 'top',
-                floating: false,
                 backgroundColor: '#FFFFFF'
             },
             series: [{
