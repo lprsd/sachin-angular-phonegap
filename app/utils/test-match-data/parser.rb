5c04640bd259f7529d5694150adbad4b47c9595c
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-02-05 17:48:33 +0530

    Motion Chart data: added top 15 players test match data cumulative

--- app/utils/test-match-data/parser.rb
+++ app/utils/test-match-data/parser.rb
@@ -135,3 +135,57 @@ require 'csv'
 # end
 
 # p india_with_sachin
+
+# # Top 15 Players Cumulative
+# # Uncomment below to run
+# # ======================
+
+def top_15_players_cumulative_runs(row)
+  [row['Start Date'], row['Runs']]
+end
+
+def top_15_players_cumulative_avg(row)
+  [row['Start Date'], row['Ave']]
+end
+
+def top_15_players_cumulative_100s(row)
+  [row['Start Date'], row['100']]
+end
+
+top_15_players_cumulative_list_runs = []
+top_15_players_cumulative_list_avg  = []
+top_15_players_cumulative_list_100s = []
+
+['Sachin Tendulkar', 'Ricky Ponting', 'Jacques Kallis', 'Rahul Dravid', 'Brian Lara', 
+  'Mahela Jayawardene', 'Shivanarine Chanderpaul', 'Allan Border', 'Steve Waugh', 
+  'Kumar Sangakkara', 'Sunil Gavaskar', 'Graeme Smith', 'Graham Gooch', 
+  'Javed Miandad', 'Inzamam-ul-Haq'].each do |name|
+    player = {
+      'name' => name
+    }
+    
+    cumulative_runs = []
+    cumulative_avg  = []
+    cumulative_100s = []
+    
+    CSV.foreach('top_15_players_cumulative.csv', headers: true) do |row|
+      if row['Name'] == name
+        player['country'] = row['Country']
+        cumulative_runs << top_15_players_cumulative_runs(row)
+        cumulative_avg << top_15_players_cumulative_avg(row)
+        cumulative_100s << top_15_players_cumulative_100s(row)
+      end
+    end
+
+    top_15_players_cumulative_list_runs << player.merge({'runs' => cumulative_runs})
+    top_15_players_cumulative_list_avg  << player.merge({'average' => cumulative_avg})
+    top_15_players_cumulative_list_100s << player.merge({'centuries' => cumulative_100s})
+  end
+
+
+p top_15_players_cumulative_list_runs
+p '*'*100
+p top_15_players_cumulative_list_avg
+p '*'*100
+p top_15_players_cumulative_list_100s
+
